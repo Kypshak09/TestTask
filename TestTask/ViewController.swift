debcbfa617a22282ed7edb7090d14d02d5491d10
Kypshak09 <amirzhunusov09@gmail.com>
2023-07-26 00:48:36 +0600

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Amir Zhunussov on 26.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
